6b8bc57ba521edc306f6df82b06e9cc5f73c6bc9
Shizukin <Jackqiu147@gmail.com>
2024-04-19 21:36:51 +0200

    Version1

--- Cinema/src/Modele/Connexion.java
+++ Cinema/src/Modele/Connexion.java
@@ -12,9 +12,9 @@ public class Connexion {
     public int getnbrplace;
     String databaseName="Cinema";
     String username="root";
-    String password="";
+    //String password="";
 
-   //// String password="Jack123456";
+    String password="Jack123456";
 
     //String password="Jack123456";
 
